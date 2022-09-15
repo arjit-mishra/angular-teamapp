5d1e4d7b248b0837295a319a3022bbe430183dcf
Arjit Mishra <arjitm786@gmail.com>
2022-09-15 13:38:23 +0530

    Done with apoplication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,68 @@ export class AppComponent {
   
   newMemberName = '';
   members: string[] = [];
-  
-  addMember(){
-    this.members.push(this.newMemberName);
-    this.newMemberName = '';
-    console.log(this.members);  
-  }
+  errorMessage = '';
+  numberOfTeams: number | '' = '';
+  teams: string[][] = [];
   
   onInput(member: string) {
     this.newMemberName = member;
-    console.log(this.newMemberName);
   }
+
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value);
+  }
+
+  addMember(){
+
+    if(!this.newMemberName){
+      this.errorMessage = "Name cannot be empty";
+      console.log(this.errorMessage);
+      return;
+    }
+
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+    this.errorMessage = '';
+  }
+  
+  generateTeams(){
+    
+    
+    if(!this.numberOfTeams || this.numberOfTeams <=0 ){
+      this.errorMessage = 'Invalid number of teams';
+      return;
+    }
+    
+    if(this.members.length < this.numberOfTeams){
+      this.errorMessage = 'Not enough members to form teams';
+      return;
+    }
+
+
+    this.errorMessage = '';
+
+    const allMembers = [...this.members];
+
+    while(allMembers.length){
+      for(let i=0; i<this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * allMembers.length);
+        const randomnMember = allMembers.splice(randomIndex,1)[0];
+
+        if(!randomnMember)
+          break;
+
+        if(this.teams[i]){
+          this.teams[i].push(randomnMember);
+        } else{
+          this.teams[i] = [randomnMember];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = '';
+
+  }
+
 }
