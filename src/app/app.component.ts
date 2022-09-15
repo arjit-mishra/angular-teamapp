7e9a4c2c54633a3c0d202e5f3ca7fa530175f40f
Arjit Mishra <arjitm786@gmail.com>
2022-09-15 12:15:00 +0530

    Angular Team App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+  
+  newMemberName = '';
+  members: string[] = [];
+  
+  addMember(){
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+    console.log(this.members);  
+  }
+  
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
 }
